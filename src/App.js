ea6a86547f3340b1ce3263115dfe574098b4fd50
Bryant <cabrera.bryant@gmail.com>
2019-04-03 16:26:02 -0700

    Updates register successfully in Register.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,63 @@ import { Switch, Route, withRouter } from 'react-router-dom'
 import Nav from './Components/Nav/Nav'
 import "semantic-ui-css/semantic.min.css";
 import Sittersindex from './Components/Sittersindex/Sittersindex'
-import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
 
 
 
 class App extends Component {
-  render() {
+    state = {
+        loggedUser: {}
+    }
 
-    return (
-      <body>
-        <Nav />
-        <Switch>
-          <Route exact path={'/home'} component={ () => <Login/>} />
-          <Route exact path={'/sitters'} component={() => <Sittersindex />} />
-        </Switch>
-      </body>
-    )
-  }
+    doLoginUser = async (user) => {
+        try {
+            const loginResponse = await fetch (
+                `${process.env.REACT_APP_API_URL}/auth/login`,
+                {
+                    method: "POST",
+                    credentials: "include",
+                    body: JSON.stringify(user),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            );
+
+            if (!loginResponse.ok) {
+                throw Error(loginResponse.statusText);
+            }
+
+            const parsedResponse = await loginResponse.json();
+
+            if (parsedResponse.message === "login successful") {
+                //Resets this component's state if a use was successfully logged in
+                this.setState({
+                    loggedUser: parsedResponse.data
+                });
+
+                this.props.history.push(`/sitters`);
+            } else {
+                this.setState({
+                    loginError: parsedResponse.message
+                });
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    render() {
+        return (
+            <body>
+                <Nav />
+                <Switch>
+                <Route exact path={'/home'} component={ () => <Register/>} />
+                <Route exact path={'/sitters'} component={() => <Sittersindex />} />
+                </Switch>
+            </body>
+        )
+    }
 }
 
-export default App;
+export default withRouter(App);
